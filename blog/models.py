6e52d6ca1b209811e35e91341ded20496782d464
kyo <odinxp@gmail.com>
2022-06-15 22:07:16 +0800

    feat: 將__str__替換為物件名

--- blog/models.py
+++ blog/models.py
@@ -4,6 +4,9 @@ from django.db import models
 class Author(models.Model):
     name = models.CharField(max_length=100)
 
+    def __str__(self):
+        return self.name
+
 
 class Post(models.Model):
     title = models.CharField(max_length=100)
@@ -11,11 +14,20 @@ class Post(models.Model):
     author = models.ForeignKey(Author, on_delete=models.CASCADE, related_name='posts')
     tags = models.ManyToManyField('Tag', related_name='posts')
 
+    def __str__(self):
+        return self.title
+
 
 class Tag(models.Model):
     name = models.CharField(max_length=100)
 
+    def __str__(self):
+        return self.name
+
 
 class Cover(models.Model):
-    url = models.CharField(max_length=100)
+    name = models.CharField(max_length=100)
     post = models.OneToOneField(Post, on_delete=models.CASCADE, related_name='cover')
+
+    def __str__(self):
+        return self.name
