d4316d1e40e24f9456b5b8d52e2b87193bee71e7
kyo <odinxp@gmail.com>
2022-06-15 21:36:08 +0800

    feat: add models

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+
+class Author(models.Model):
+    name = models.CharField(max_length=100)
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    author = models.ForeignKey(Author, on_delete=models.CASCADE, related_name='posts')
+    tags = models.ManyToManyField('Tag', related_name='posts')
+
+
+class Tag(models.Model):
+    name = models.CharField(max_length=100)
+
+
+class Cover(models.Model):
+    url = models.CharField(max_length=100)
+    post = models.OneToOneField(Post, on_delete=models.CASCADE, related_name='cover')
